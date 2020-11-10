cdb4548797fd9b21ad0f8f45c056bd1c18acdddc
shivatejapeddi <shivatejapeddi@gmail.com>
2020-11-10 19:24:09 +0530

    Revert "sepolicy: Add hal_lineage_fod domain"
    This reverts commit c1849d6946db5ef8f3bb2c6dc79439e9b07b2d93.

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -9,17 +9,12 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/aosp/sepolicy/common/public
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+#    device/aosp/sepolicy/common/public
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aosp/sepolicy/common/private
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+#    device/aosp/sepolicy/common/private
 
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aosp/sepolicy/common/dynamic
-else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/aosp/sepolicy/common/dynamic \
     device/aosp/sepolicy/common/vendor
-endif
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -9,14 +9,8 @@ TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/aosp/sepolicy/qcom/dynamic
-else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/qcom/dynamic \
     device/aosp/sepolicy/qcom/vendor
-endif
 
 ifeq (,$(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
 BOARD_SEPOLICY_M4DEFS += \
